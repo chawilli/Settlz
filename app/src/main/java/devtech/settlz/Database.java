fb8299f87ba3f3f06ba4178a59cdcb2a3bebc493
Jorelle Bantog <williamchan200@gmail.com>
2016-11-10 20:58:22 -0500

    Jorelle Bantog - UI change for Register

--- app/src/main/java/devtech/settlz/Database.java
+++ app/src/main/java/devtech/settlz/Database.java
@@ -195,11 +195,19 @@ public class Database {
     public int register(String email, String password) {
         try {
             PreparedStatement preparedStmt = null;
-            String query="INSERT INTO Users (Password, Email) " +
-                     "VALUES (?, ?);";
+            String query="INSERT INTO Users (Password, Email, Created) " +
+                     "VALUES (?, ?, ?);";
+
+            Calendar c = Calendar.getInstance();
+            int year = c.get(Calendar.YEAR);
+            int month = c.get(Calendar.MONTH)+1;
+            int day = c.get(Calendar.DAY_OF_MONTH);
+            String dateCreated = year+"-"+month+"-"+day;
+
             preparedStmt = conn.prepareStatement(query);
             preparedStmt.setString(1,password);
             preparedStmt.setString(2,email);
+            preparedStmt.setString(3,dateCreated);
             preparedStmt.executeQuery();
 
             String userid = "SELECT UserId " +
@@ -449,9 +457,8 @@ public class Database {
             int year = c.get(Calendar.YEAR);
             int month = c.get(Calendar.MONTH)+1;
             int day = c.get(Calendar.DAY_OF_MONTH);
-
-            Log.d("CREATETEST","I GOT TO HERE");
             String dateCreated = year+"-"+month+"-"+day;
+
             String insertPollsQuery = "INSERT INTO Polls (Argument,ReportCount,PollStatus,ExpiryDate,CategoryCategoryId,Option_OptionsId,Twitter_TwitterId,Facebook_FacebookId, User_UserId, DateCreated) " +
                     "VALUES('"+argument+"',0,'True','"+expiryDate+"',"+categoryId+","+optionsId+","+twitterId+","+facebookId+","+userId+", '"+dateCreated+"');";
             String getPollsQuery = "SELECT TOP 1 PollId FROM Polls " +
@@ -462,7 +469,6 @@ public class Database {
             rs = stmt.executeQuery(getPollsQuery);
             rs.next();
             int pollId = rs.getInt("PollId");
-            Log.d("CREATETEST","I GOT TO HERE2");
 
             String insertPollUserQuery= "INSERT INTO PollUser (Polls_PollId, Users_UserId) VALUES("+pollId+", "+userId+");";
             stmt = conn.createStatement();
--- app/src/main/java/devtech/settlz/MainActivity.java
+++ app/src/main/java/devtech/settlz/MainActivity.java
@@ -115,6 +115,8 @@ public class MainActivity extends AppCompatActivity
     }
 
 
+
+
     @Override
     public void onBackPressed() {
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
